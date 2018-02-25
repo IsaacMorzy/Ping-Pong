292a3dbb139c75fb7979ba5846b7c6c4820c4c46
Isaac Musyoka <musyokaisaac98@gmail.com>
2018-02-26 01:45:04 +0300

    Make changes to the UserInterface &  business logic

--- js/png.js
+++ js/png.js
@@ -1,30 +1,30 @@
 // //Business logic
- function ping(input) {
+function ping(input) {
     for (var i = 1; i <= input; i++) {
-        if ((i % 3 === 0) && (i % 5 === 0)) {
-           $(".output").append("<li"+"pinpong"+"</li>");
+        if (i % 15 === 0) {
+            $(".output").append("<li" + "pinpong" + "</li>");
         } else if (i % 3 === 0) {
-             $(".output").append("<li>"+"ping"+"</li>");
+            $(".output").append("<li>" + "ping" + "</li>");
 
         } else if (i % 5 === 0) {
 
-            $(".output").append("<li>" +"pong"+"</li>");
+            $(".output").append("<li>" + "pong" + "</li>");
         } else {
 
-             $(".output").append("<li>" +i+"</li>");
-         }
+            $(".output").append("<li>" + i + "</li>");
+        }
 
-   }
- }
+    }
+}
 
 /* Interface logic*/
 
-$(document).ready(function(){
-  $("#inputno").submit(function(){
+$(document).ready(function() {
+    $("#inputno").submit(function() {
         event.preventDefault();
-    var input=parseInt($("#inputNumber").val());
-    ping(input);
+        var input = parseInt($("#inputNumber").val());
+        ping(input);
 
 
-  });
     });
+});
