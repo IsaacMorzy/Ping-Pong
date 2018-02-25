34bc952feb0996c97d9559ae8c369a5bf8b14ea5
Isaac Musyoka <musyokaisaac98@gmail.com>
2018-02-25 03:02:37 +0300

    Add the user interface with business logic

---
+++ js/png.js
@@ -0,0 +1,30 @@
+// //Business logic
+ function ping(input) {
+    for (var i = 1; i <= input; i++) {
+        if ((i % 3 === 0) && (i % 5 === 0)) {
+           $(".output").append("<li"+"pinpong"+"</li>");
+        } else if (i % 3 === 0) {
+             $(".output").append("<li>"+"ping"+"</li>");
+
+        } else if (i % 5 === 0) {
+
+            $(".output").append("<li>" +"pong"+"</li>");
+        } else {
+
+             $(".output").append("<li>" +i+"</li>");
+         }
+
+   }
+ }
+
+/* Interface logic*/
+
+$(document).ready(function(){
+  $("#inputno").submit(function(){
+        event.preventDefault();
+    var input=parseInt($("#inputNumber").val());
+    ping(input);
+
+
+  });
+    });
